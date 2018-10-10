afee20bc7175cfd60ddfd9adb82ac3c0e7b6b2e4
Thomas Rynne <thomas.rynne@digital.hmrc.gov.uk>
2018-10-10 10:25:50 +0100

    [TR] TXM-3285 sbt changes for new jenkins build server

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -16,7 +16,10 @@
 
 import sbt.Keys._
 import sbt._
+import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
 import uk.gov.hmrc.versioning.SbtGitVersioning
+import uk.gov.hmrc.SbtArtifactory
 
 object HmrcBuild extends Build {
 
@@ -25,14 +28,13 @@ object HmrcBuild extends Build {
   val appName = "logging"
 
   lazy val library = Project(appName, file("."))
-    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning, SbtArtifactory)
     .settings(
+      majorVersion := 0,
       scalaVersion := "2.11.8",
       libraryDependencies ++= AppDependencies(),
       crossScalaVersions := Seq("2.11.8"),
-      parallelExecution in Test := false,
-      resolvers := Seq(
-        Resolver.bintrayRepo("hmrc", "releases"), "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/")
+      parallelExecution in Test := false
     )
 }
 
